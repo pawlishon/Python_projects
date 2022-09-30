e84299fdeae0d65ee3e3441db64434535b091aae
pawlishon <paw.piatkowski7@gmail.com>
2022-09-30 20:10:06 +0200

    changing db to mysql (deleted password and username for db conn)

--- Cookbook/Cookbook/Cookbook/settings.py
+++ Cookbook/Cookbook/Cookbook/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'Cookbook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RECIPIES_DJANGO',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '89.69.154.102',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
