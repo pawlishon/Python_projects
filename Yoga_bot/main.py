6473381c953699c129e5f74e80852b44fc2fe4b3
pawlishon <paw.piatkowski7@gmail.com>
2023-02-20 16:14:01 +0100

    Adding multithreading to yoga bot and now the program also constantly tries to sign up for class for specified amount of time.

--- Yoga_bot/main.py
+++ Yoga_bot/main.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdriver, common
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import datetime
@@ -6,8 +6,17 @@ from dateutil.relativedelta import relativedelta
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 import sys
+import threading
 
+
+# Please remove '_template' from reservations_template.txt after you fill it with your data.
+# Variable below refers to number of minutes the script will be trying to sign you in to the class
+MINUTE_THRESHOLD = 3
+
+##########################################################################################
+time_threshold = MINUTE_THRESHOLD * 60
 sys.stdout = open(f"log{datetime.date.today().strftime('%Y%m%d')}.txt", 'wt')
+
 
 class User:
     def __init__(self, user_name, password):
@@ -37,25 +46,17 @@ def get_class_link(yoga_class, soup):
                 parent_div = classes.parent
                 a = parent_div.attrs.keys()
                 if 'data-link' in list(parent_div.attrs.keys()):
-                    if (yoga_class.time in parent_div.find('div', {'class': 'list-entry-time'}).text) & (yoga_class.date in parent_div.attrs['data-link']):
+                    if (yoga_class.time in parent_div.find('div', {'class': 'list-entry-time'}).text) & \
+                            (yoga_class.date in parent_div.attrs['data-link']):
                         return parent_div.attrs['data-link']
 
 
-print('Initialize driver')
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
-
-print('Reading reservation file:')
-with open('reservations.txt', 'r') as f:
-    content = f.readlines()
-
-for row in content:
-    param_list = row.replace('\n', '').split(',')
-    my_user = User(param_list[0], param_list[1])
-    yoga_class = YogaClass(param_list[2], param_list[3], param_list[4], param_list[5])
-
+def sign_for_class(my_user, yoga_class):
+    print('Initialize driver')
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
     print('Logging')
     driver.get('https://studiojogapark.pl/strefaklienta/index.php?s=logowanie')
     driver.find_element(value='email').send_keys(my_user.user_name)
@@ -68,7 +69,45 @@ for row in content:
     driver.get('https://studiojogapark.pl/strefaklienta/' + get_class_link(yoga_class, soup))
     radio_button = driver.find_element(By.XPATH, '//*[@id="usluga15760"]')
     driver.execute_script("arguments[0].click();", radio_button)
-    driver.find_element(By.XPATH, '//*[@id="f-submit"]/button').click()
+    success = False
+    seconds_past = 0
+    start_time = datetime.datetime.strptime(param_list[2] + ',' + param_list[5], '%Y-%m-%d,%H:%M')
+    week = 7 * 24 * 60 * 60
+    while (not success) and (seconds_past < time_threshold):
+        try:
+            driver.find_element(By.XPATH, '//*[@id="f-submit"]/button').click()
+            print(f'{my_user.user_name} signed in at {datetime.datetime.now()}')
+            success = True
+        except common.exceptions.NoSuchElementException:
+            driver.refresh()
+            radio_button = driver.find_element(By.XPATH, '//*[@id="usluga15760"]')
+            driver.execute_script("arguments[0].click();", radio_button)
+            seconds_past = (datetime.datetime.now() - start_time).total_seconds()
+            print(f'Waiting for class to appear still {(-1 * seconds_past) - week} seconds to start time.')
+
+    if seconds_past >= time_threshold:
+        print('Program timed out')
 
     print('Logging out')
     driver.get('https://studiojogapark.pl/strefaklienta/index.php?a=wyloguj')
+
+print('Reading reservation file:')
+with open('reservations.txt', 'r') as f:
+    content = f.readlines()
+
+threads = []
+for row in content:
+    param_list = row.replace('\n', '').split(',')
+    my_user = User(param_list[0], param_list[1])
+    yoga_class = YogaClass(param_list[2], param_list[3], param_list[4], param_list[5])
+    print(f'Creating thread for {param_list[0]} to join {param_list[5]}')
+    thread = threading.Thread(target=sign_for_class, args=(my_user, yoga_class))
+    threads.append(thread)
+
+print(f'Threads started {len(threads)}')
+for job in threads:
+    job.start()
+
+for job in threads:
+    job.join()
+print('Finished for all users and classes')
